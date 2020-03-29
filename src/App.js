a428cf2bdd0f7f3fa616301be65fb6eb0008b23f
Daniel Lwetabe <acehanks@gmail.com>
2020-03-29 11:22:17 +0300

    onChange event for Search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Search from './components/Search';
 
 function App() {
+  const apiUrl = "http://www.omdbapi.com/?apikey=aefdd5fb";
+  
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
+  const handleChange = (e) => {
+    let s = e.target.value;
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+    // console.log(state.s);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie App</h1>
       </header>
       <main>
-        <Search />
+        <Search handleChange={handleChange} />
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Search() {
+function Search({handleChange}) {
     return (
         <section className="searchbox-wrap">
             <input type="text" 
-            name="" 
+            name="search" 
             className="searchbox" 
-            placeholder="Search for a movie" 
+            placeholder="Search for a movie"
+            onChange = {handleChange}
             />
         </section>
     )
